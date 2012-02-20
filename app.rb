1e41521efab275d459385d9a03e230195902555c
Ryan Daigle <ryan.daigle@gmail.com>
2012-02-20 12:49:34 -0500

    add TOC generation to app

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
+require 'nokogiri'
+
+Tilt.prefer Tilt::MarukuTemplate
+
 module Nesta
   class App
 
     # Route to theme assets
     use Rack::Static, :urls => ["/clean", "/favicon.ico", "/apple-touch-icon.png", "/apple-touch-icon-precomposed.png"], :root => "themes/clean/public"
+
+    # Provide page TOC    
+    def toc(page, toc_template = :table_of_contents)
+      toc_headers = Nokogiri::HTML(page.body).css('h2').inject({}) do |mappings, header_node|
+        mappings[header_node.attr('id')] = header_node.content
+        mappings
+      end
+      haml(toc_template, :layout => false, :locals => { :toc_headers => toc_headers })
+    end
   end
 end
